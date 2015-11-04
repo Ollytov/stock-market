7a8b9e9ef2a62e395b7a5e7c0117bacfbf271a4d
Ollytov <daniel.maloof@gmail.com>
2015-11-03 20:53:57 -0500

    initial commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://meanuser:meanpass@ds049104.mongolab.com:49104/heroku_17jlwb0b'
+  },
+
+  seedDB: true
+};
---
+++ server/config/environment/production.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Production specific configuration
+// =================================
+module.exports = {
+  // Server IP
+  ip:       process.env.OPENSHIFT_NODEJS_IP ||
+            process.env.IP ||
+            undefined,
+
+  // Server port
+  port:     process.env.OPENSHIFT_NODEJS_PORT ||
+            process.env.PORT ||
+            8080,
+
+  // MongoDB connection options
+  mongo: {
+    uri:    process.env.MONGOLAB_URI ||
+            process.env.MONGOHQ_URL ||
+            process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
+            'mongodb://meanuser:meanpass@ds049104.mongolab.com:49104/heroku_17jlwb0b'
+  }
+};
